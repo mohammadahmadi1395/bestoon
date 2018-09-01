126a71d280b0182bdcd0b42f457e93cd1befc9f7
Mohammad <ahmadi.mohammad2008@gmail.com>
2018-09-01 11:35:29 +0430

    Income Creation

--- web/models.py
+++ web/models.py
@@ -9,3 +9,10 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
