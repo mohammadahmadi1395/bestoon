637c29937832622cf4d887d969a709c663515f60
Mohammad <ahmadi.mohammad2008@gmail.com>
2018-09-01 19:28:37 +0430

    sumbit/income

--- web/views.py
+++ web/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
+from web.models import Token, User, Expense, Income
+from datetime import datetime
+
+from json import JSONEncoder
+from django.http import JsonResponse
+#برای آن که دچار حملات
+#DDOS
+#نشویم، برنامه اجبار میکند که حتما درخواست از سوی فرم انجام شود
+# این دو خط برای این است که بی‌خیال این مسیله شود و اجاز دهد که از طریق غیرفرم هم درخواست انجام شود
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+
+
 # Create your views here.
+
+def submit_expense(request):
+    """submits an expense"""
+    now = datetime.now()
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=now)
+
+    return JsonResponse(
+    {
+        'status':'ok',
+    },
+    encoder=JSONEncoder
+    )
+
+@csrf_exempt
+def submit_income(request):
+    """submits an income"""
+    now = datetime.now()
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    Income.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=now)
+
+    return JsonResponse(
+    {
+        'status':'ok',
+    },
+    encoder=JSONEncoder
+    )
